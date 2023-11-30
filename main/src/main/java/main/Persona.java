459b652723376304438d05286fb02665ae100c3a
sayo-melocojon <145472048+sayo-melocojon@users.noreply.github.com>
2023-11-30 12:18:49 -0600

    Add files via upload
    Programa con las clases empleado y persona

---
+++ main/src/main/java/main/Persona.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author User sayo melocojon
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String Genero;
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre=nombre;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad =edad;
+    }
+    
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero= Genero;
+    }
+    
+    public void mostrarDatos (){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Edad: "+edad);
+       System.out.println("Genero: "+Genero); 
+    }
+}
